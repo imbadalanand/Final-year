4d94e1a91ff8f490782d22af5fd8f064e7450eaf
Badal <badaldj101@gmail.com>
2023-05-17 12:59:06 +0530

    adding books

--- 2ndhand-book/src/App.js
+++ 2ndhand-book/src/App.js
@@ -1,15 +1,33 @@
 
 import './App.css';
-
+import data from './data';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
+   
     <div>
       <header>
-        <a href="/">2ndHanddd Book</a>
+        <a href="/">Old Book</a>
       </header>
-      <main>list products</main>
+      <main>
+         <h1>Featured Books</h1>
+        <div className="products">
+        {
+        data.products.map(product => (
+        <div className='product' key={product.key}>
+          <img src={product.image} alt={product.name}/>
+          <p>{product.name}</p>
+          <p>{product.language},{product.category},{product.author}</p>
+          <p> <i class="fa fa-inr"></i>{product.price}</p>
+
+        </div>))
+        }
+        </div>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
