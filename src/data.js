d013c1b38cc838e851b01a2fc47ea67b012de617
Hemant Raj <hemantraj202@gmail.com>
2023-05-20 13:09:05 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,112 @@
+const data={
+    products : [
+        {
+            name: 'Political Science',
+            slug: 'political-science',
+            language:'Hindi',
+            category: 'Comparative Politics',
+            image:'/images/polscience.jpg',
+            price: 120,
+            countInStock: 2,
+            author:'Dr. Pukhraj Jain',
+            rating: 4.0,
+            numReviews: 10,
+            description:'Political Science Syllabus For B.A. Semester I of Lucknow University',
+            publisher:'Bharti Bhawan'
+        },
+        {
+            name: 'Losing Hope',
+            slug: 'political-science',
+            language:'English',
+            category: 'Romance',
+            image:'/images/losing-hope.jpg',
+            price: 60,
+            countInStock: 1,
+            author:'Mr. Hoover Collen',
+            rating: 5.0,
+            numReviews: 5,
+            description:"Sky returns from the events outlined in Hopeless. She has been baring everything, secrets, feelings and memories, but Holder is still hiding his past. He continues to be haunted by the young girl he let walk away, and he looks for her, trying to free himself of the guilt. However, he has no idea that when he finds her, he will also find greater melancholy. In this breath-taking sequel to the bestseller, Colleen Hoover reveals the darkness behind Holders past"
+        },
+        {
+            name: 'The Bombing War',
+            slug: '-bomb-war',
+            language:'English',
+            category: 'WAR',
+            image:'/images/thebombing.jpg',
+            price: 420,
+            countInStock: 1,
+            author:'Overy Richard',
+            rating: 4.0,
+            numReviews: 3,
+            description:'The ultimate history of the Blitz and bombing in the Second World War, from Wolfson Prize-winning historian and author Richard Overy The use of massive fleets of bombers to kill and terrorize civilians was an aspect of the Second World War which continues to challenge the idea that Allies specifically fought a moral war.'
+        },
+        {
+            name: 'India is My Dream',
+            slug: 'political-science',
+            language: 'Hindi',
+            category: 'Biography',
+            image:'/images/gandhi.jpg',
+            price: 65,
+            countInStock: 1,
+            author:'M. K. Gandhi',
+            rating: 5.0,
+            numReviews: 3,
+            description:'This book is a collection of passages from speeches and writings of Gandhi showcasing how he envisaged India. Enumerating his views and ideas on various socio-political, cultural and economic issues, the book renders a fascinating insight into Gandhis ideologies and his dream of a prosperous and vibrant India'
+        },
+        {
+            name: 'Political Science',
+            slug: 'political-science',
+            language:'Hindi',
+            category: 'Comparative Politics',
+            image:'/images/polscience.jpg',
+            price: 120,
+            countInStock: 2,
+            author:'Dr. Pukhraj Jain',
+            rating: 4.0,
+            numReviews: 10,
+            description:'Political Science Syllabus For B.A. Semester I of Lucknow University',
+            publisher:'Bharti Bhawan'
+        },
+        {
+            name: 'Losing Hope',
+            slug: 'political-science',
+            language:'English',
+            category: 'Romance',
+            image:'/images/losing-hope.jpg',
+            price: 60,
+            countInStock: 1,
+            author:'Mr. Hoover Collen',
+            rating: 5.0,
+            numReviews: 5,
+            description:"Sky returns from the events outlined in Hopeless. She has been baring everything, secrets, feelings and memories, but Holder is still hiding his past. He continues to be haunted by the young girl he let walk away, and he looks for her, trying to free himself of the guilt. However, he has no idea that when he finds her, he will also find greater melancholy. In this breath-taking sequel to the bestseller, Colleen Hoover reveals the darkness behind Holders past"
+        },
+        {
+            name: 'The Bombing War',
+            slug: 'political-science',
+            language:'English',
+            category: 'WAR',
+            image:'/images/thebombing.jpg',
+            price: 420,
+            countInStock: 1,
+            author:'Overy Richard',
+            rating: 4.0,
+            numReviews: 3,
+            description:'The ultimate history of the Blitz and bombing in the Second World War, from Wolfson Prize-winning historian and author Richard Overy The use of massive fleets of bombers to kill and terrorize civilians was an aspect of the Second World War which continues to challenge the idea that Allies specifically fought a moral war.'
+        },
+        {
+            name: 'India is My Dream',
+            slug: 'political-science',
+            language: 'Hindi',
+            category: 'Biography',
+            image:'/images/gandhi.jpg',
+            price: 65,
+            countInStock: 1,
+            author:'M. K. Gandhi',
+            rating: 5.0,
+            numReviews: 3,
+            description:'This book is a collection of passages from speeches and writings of Gandhi showcasing how he envisaged India. Enumerating his views and ideas on various socio-political, cultural and economic issues, the book renders a fascinating insight into Gandhis ideologies and his dream of a prosperous and vibrant India'
+        }
+    ]
+}
+
+export default data;
