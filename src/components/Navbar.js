08758ab171b8dec1cb89509bb1ba2f6fa0376cea
Harshit99312 <harshitsharma99312@gmail.com>
2023-06-23 12:01:45 +0530

    book selling app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,86 +1,169 @@
-import React from 'react'
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { updateFilteredProduct } from "../redux/actions/product";
+import Login from "./Login";
+import { Nav, NavDropdown } from "react-bootstrap";
 
-const AdminLogin = () => {
 
-  const [id, idupdate] = useState("");
-  const [password, passwordupdate] = useState("");
+const Navbar = () => {
+    
+  const dispatch = useDispatch();
+  const allProduct = useSelector((s) => s.cartReducer.allProduct);
+  const navigate = useNavigate();
 
-  const usenavigate=useNavigate()
+  const filterResult = (catItem) => {
+    const result = allProduct.filter((curData) => {
+      return curData.category === catItem;
+    });
+    console.log("result", result);
+    dispatch(updateFilteredProduct(result));
+  };
 
-  const ProceedLogin = (e) => {
-    e.preventDefault();
-    if(validate()){
-      //implementation
-      //console.log('proceed');
-      fetch("http://localhost:5000/admin/"+id).then((res)=>{
-        return res.json();
-      }).then((resp)=>{
-        console.log(resp)
-        if(Object.keys(resp).length===0){
-          alert('Please Enter valid username');
-        }else{
-          if (resp.id === id && resp.password === password){
-            alert('Success');
-            localStorage.setItem("user", JSON.stringify(resp));
-            localStorage.setItem("loggedin", true);
-            usenavigate('/Admin')
-          }else{
-            alert('Please Enter valid credentials');
-          }
+  const [searchid, searchupdate] = useState("");
 
-        }
-      }).catch((err)=>{
-        alert.error('Login Failed due to:'+err.message);
-      });
 
-    }
-  }
-  const validate =()=>{
-    let result=true;
-    if(id ==='' || id===null){
-      result=false;
-      alert('Please Enter Username');
-      
-    }
-    if(password ==='' || password===null){
-      result=false;
-      alert('Please Enter Password');
-     
+  const userInfo = JSON.parse(localStorage.getItem("user"));
 
-    }
-    return result;
-  }
+  const handleLogout = () => {
+    localStorage.removeItem("loggedin");
+    localStorage.removeItem("user");
+    localStorage.removeItem("admin");
+    navigate("/");
+  };
 
   return (
-    <div className="row">
-      <div className="offset-lg-3 col-lg-6">
-        <form onSubmit={ProceedLogin} className="container">
-          <div className="card">
-            <div className="card-header">
-              <h1>Admin Login</h1>
-            </div>
-            <div className="card-body">
-              <div className="form-group">
-                <label>
-                  Admin User ID <span className="errmsg">*</span>
-                </label>
-                <input value={id} onChange={e=>idupdate(e.target.value)} className="form-control"></input>
-              </div>
-              <div className="form-group">
-                <label>Password <span className="errmsg">*</span></label>
-                <input type="password" value={password} onChange={e=>passwordupdate(e.target.value)} className="form-control"></input>
-              </div>
-            </div>
-            <div className="card-footer">
-              <button type="submit" className="btn btn-primary" >Admin Login</button>
-             
-            </div>
+    <>
+      
+      <nav className="navbar navbar-expand-lg bg-secondary text-white">
+        <div className="container-fluid" style={{ color: "white" }}>
+          <img src="../images/logo.png" style={{ width: "70px", height: "60px" }} alt="" />
+          <Link className="navbar-brand" to="/" style={{ color: "white" }}>
+            Refurbished Book Store
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/" style={{ color: "white" }}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ color: "white" }}>
+                  Feature Books
+                </Link>
+                <ul className="dropdown-menu">
+                <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("UPDATE_ALL_PRODUCT")}>
+                      All Books
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Fiction")}>
+                      Fiction
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Science")}>
+                      Science
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Religious")}>
+                      Religious
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Romance")}>
+                      Romance
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Biography")}>
+                      Biography
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" onClick={() => filterResult("Finance")}>
+                      Finance
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Contact" style={{ color: "white" }}>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                value={searchid}
+                onChange={(e) => searchupdate(e.target.value)}
+                className="form-control me-3"
+                type="text"
+                id="userInput"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <Link to={`/Search/${searchid}`}>
+                <button className="btn btn-light" type="submit">
+                  Search
+                </button>
+              </Link>
+            </form>
+
+            <Link to="/Cart">
+              <i className="fa-solid fa-cart-shopping" style={{ margin: "10px 10px", color: "white" }}></i>
+            </Link>
+
+          { userInfo? (
+            <NavDropdown title={userInfo.name} id="username">
+                <Link to='/profile'>
+                    <NavDropdown.Item>
+                        
+                        <Link to="/UserProfile/accountsettings">Profile</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={handleLogout}>
+                        Logout
+                    </NavDropdown.Item>
+                </Link>
+
+            </NavDropdown>
+          ) : (
+            <Link to='/Login'>
+               <i className="fa-solid fa-user" style={{ margin: "10px 10px", color: "white" }}>&nbsp; Sign In</i>
+                
+                
+            
+            </Link>
+          )}
+
+            
           </div>
-        </form>
+        </div>
+      </nav>
+      <div className="admin" style={{ position: "absolute", top: "1px", right: "1px", border: "cover" }}>
+        <Link to="/AdminL">Admin</Link> 
       </div>
-    </div>
+    </>
   );
 };
-export default AdminLogin;
+
+export default Navbar;
