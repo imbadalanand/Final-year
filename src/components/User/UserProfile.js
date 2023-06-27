f8a3a4ffc9b06d7b1ec75487dbeac5ea06636fc3
Badal Kumar <badaldj101@gmail.com>
2023-06-27 22:42:08 +0530

    admin login problem user profile prevent for opening

--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import UserSidebar from "./UserSidebar";
 import AccountSettings from "./AccountSettings";
 import "./userprofile.css";
+import ChangePassword from "./ChangePassword";
+
 
 const UserProfile = () => {
+  const usenavigate = useNavigate();
+ 
   const { activepage } = useParams();
-  alert(activepage);
+  const userInfo = JSON.parse(localStorage.getItem("Aloggedin"));
+ 
+  if (userInfo){
+    alert("You are not authorised");
+  }
+  else{
+   
+  }
+  
+  
+  
   return (
     <div className="userprofile">
-      <h1>My Profile 1</h1>
+
+
+     
+      <h1>My Profile</h1>
 
       <div className="userprofilein">
         <div className="left">
@@ -17,6 +34,7 @@ const UserProfile = () => {
         </div>
         <div className="right">
           {activepage === "accountsettings" && <AccountSettings />}
+          {activepage === "changepassword" && <ChangePassword />}
         </div>
       </div>
     </div>
